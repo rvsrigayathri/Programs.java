5e6c0eb73eddf75c5ec804527e4736e959e69f65
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-22 06:17:36 +0530

    Add files via upload

---
+++ Primitive1.java
@@ -0,0 +1,23 @@
+package myprograms;
+
+class Primitive1 {
+	public static void main(String args[]) {
+		int a=100;
+		byte b=1;
+		short c=2;
+		long d=9987654321l;
+		float e=99.9f;
+		double f=9.998765432d;
+		char g='R';
+		boolean isData=true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(isData);
+	}
+
+}
