e7a945efa945496291f568a8162041e41fd19545
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-22 06:34:04 +0530

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,14 @@
+package myprograms;
+
+class Studentdetails {
+	public static void main(String args[]) {
+		String name="Srigayathri";
+		long register_no=72345234l;
+		String dept="ECE";
+		float cgpa=9.23f;
+		System.out.println(name);
+		System.out.println(register_no);
+		System.out.println(dept);
+		System.out.println(cgpa);
+	}
+}
