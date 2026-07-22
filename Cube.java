96a7e147dd1610b2002c701046c9ef5adf8adfc7
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-22 07:06:42 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,12 @@
+package myprograms;
+import java.util.Scanner;
+class Cube {
+
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		int num=a.nextInt();
+		int cube=num*num*num;
+		System.out.println(cube);
+	}
+
+}
