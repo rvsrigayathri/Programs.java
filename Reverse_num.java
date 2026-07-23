c0ba60ef00ae0eb25ce2fd75644d2bfde16d0117
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-23 10:14:06 +0530

    Add files via upload

---
+++ Reverse_num.java
@@ -0,0 +1,40 @@
+package myprograms;
+import java.util.Scanner;
+public class Reverse_num {
+
+	public static void Palin_num(int num) {
+		int rem=0;
+		int rev=0;
+		while(num>0) {
+			rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+		}
+		System.out.print(rev);
+	}
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		int num=a.nextInt();
+		Palin_num(num);
+	}
+
+}
+
+/*
+import java.util.Scanner;
+public class Reverse_num{
+public static void main(String args[]){
+	Scanner a=new Scanner(System.in);
+	int num=a.nextInt();
+	int rev=0;
+	while(num>0){
+		int rem=num%10;
+		rev = rev*10+rem;
+		num=num/10;
+	}
+	System.out.println(rev);
+}
+}	
+*/
+
+
