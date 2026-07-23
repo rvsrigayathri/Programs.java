c88970f37d551d588032829b5279cc0460834f22
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-23 11:45:20 +0530

    Add files via upload

---
+++ Even_digit_print.java
@@ -0,0 +1,21 @@
+package myprograms;
+import java.util.Scanner;
+public class Even_digit_print {
+
+	public static void Even(int num) {
+		while(num>0) {
+			int rem=num%10;
+			if(rem%2!=0) {
+				System.out.println(rem);
+			}
+			num=num/10;
+		}
+	}
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		int num=a.nextInt();
+		
+		Even(num);
+	}
+
+}
