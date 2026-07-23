093fd6e9473baf31a31883fb007a257b781b6b9d
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-23 11:36:37 +0530

    Add files via upload

---
+++ Odd_count.java
@@ -0,0 +1,22 @@
+package myprograms;
+import java.util.Scanner;
+public class Odd_count {
+
+	public static void Even(int num,int count) {
+		while(num>0) {
+			int rem=num%10;
+			if(num%2!=0) {
+				count=count+1;
+			}
+			num=num/10;
+		}
+		System.out.println(count);
+	}
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		int num=a.nextInt();
+		int count=0;
+		Even(num,count);
+	}
+
+}
