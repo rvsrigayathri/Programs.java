942a00a277f60396e81e89ff224eaaff5556818b
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-28 06:57:52 +0530

    Add files via upload

---
+++ DecimalToOctal.java
@@ -0,0 +1,22 @@
+package myprograms;
+import java.util.Scanner;
+class DecimaltoOctal {
+    
+    public static void Octal(int num,int count){
+        int pow=1;
+        while(num>0){
+            int rem=num%8;
+            count=count+rem*pow;
+            pow=pow*10;
+            num=num/8;
+        }
+        System.out.println(count);
+    }
+    
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        int num=a.nextInt();
+        int count=0;
+        Octal(num,count);
+    }
+}
