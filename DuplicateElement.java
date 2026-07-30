b2fbd037042edbe670f4d07d1333060e0ba20889
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-30 14:05:47 +0530

    Add files via upload

---
+++ DuplicateElement.java
@@ -0,0 +1,34 @@
+package myprograms;
+import java.util.Scanner;
+public class DuplicateElement {
+
+	public static void DuplicateElement(int arr[],int size) {
+		boolean visit[]=new boolean[size];
+		for(int i=0;i<size;i++) {
+			if(visit[i]) {
+				continue;
+			}
+			int count=1;
+			for(int j=i+1;j<size;j++) {
+				if(arr[i]==arr[j]) {
+					count=count+1;
+					visit[i]=true;
+				}
+			}
+			if(count>1) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		int size=a.nextInt();
+		int arr[]=new int[size];
+		for(int i=0;i<size;i++) {
+			arr[i]=a.nextInt();
+		}
+		DuplicateElement(arr,size);
+	}
+
+}
