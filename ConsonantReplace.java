f202d49c14f981db9142cfb75839ec507c4b4864
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-08-08 22:43:19 +0530

    Add files via upload

---
+++ ConsonantReplace.java
@@ -0,0 +1,25 @@
+package myprograms;
+import java.util.Scanner;
+public class ConsonantReplace {
+	public static void main(String args[]) {
+		Scanner a=new Scanner(System.in);
+		String s=a.nextLine();
+		Replace(s);
+	}
+	public static void Replace(String s) {
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')) {
+				if(!(ch=='A'||ch=='a'||ch=='E'||ch=='e'||ch=='I'||ch=='i'||ch=='O'||ch=='o'||ch=='U'||ch=='u')) {
+					System.out.print('&');
+				}
+				else {
+					System.out.print(ch);
+				}
+			}
+			else {
+			System.out.print(ch);
+			}
+		}
+	}
+}
